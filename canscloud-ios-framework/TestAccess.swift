7367d7b0c4d2c2cb62592e9fb573f51c035505be
phucanscloud <90325587+phucanscloud@users.noreply.github.com>
2022-10-03 16:25:37 +0700

    WIP: Updated Test Access

---
+++ canscloud-ios-framework/TestAccess.swift
@@ -0,0 +1,30 @@
+//
+//  TestAccess.swift
+//  canscloud-ios-framework
+//
+//  Created by Siraphop Chaisirikul on 3/10/2565 BE.
+//
+
+import Foundation
+
+class AllowAccess {
+    func display() {
+        print("Can Access")
+    }
+    
+    private func doNotDisplay() {
+        print("Can not Access")
+    }
+}
+
+class NotAllowAccess {
+    func display() {
+        print("Can Access")
+    }
+    
+    private func doNotDisplay() {
+        print("Can not Access")
+    }
+}
+
+

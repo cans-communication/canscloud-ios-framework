d80b516ce484b05f4afe27fcfb4e736f09af4658
phucanscloud <90325587+phucanscloud@users.noreply.github.com>
2022-11-22 10:31:01 +0700

    New project ObjC

---
+++ CansConnect/CansConnect/CansConnect.h
@@ -0,0 +1,18 @@
+//
+//  CansConnect.h
+//  CansConnect
+//
+//  Created by Siraphop Chaisirikul on 22/11/2565 BE.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CansConnect.
+FOUNDATION_EXPORT double CansConnectVersionNumber;
+
+//! Project version string for CansConnect.
+FOUNDATION_EXPORT const unsigned char CansConnectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CansConnect/PublicHeader.h>
+
+

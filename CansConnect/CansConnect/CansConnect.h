ee1d6fc7f1d1bad8c641c083d80b10b145f6b7b7
phucanscloud <90325587+phucanscloud@users.noreply.github.com>
2022-11-25 17:50:51 +0700

    DONE: Updated CansConnect.h

--- CansConnect/CansConnect/CansConnect.h
+++ CansConnect/CansConnect/CansConnect.h
@@ -14,5 +14,3 @@ FOUNDATION_EXPORT double CansConnectVersionNumber;
 FOUNDATION_EXPORT const unsigned char CansConnectVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <CansConnect/PublicHeader.h>
-
-#import <CansConnect/PublicHeader.h>

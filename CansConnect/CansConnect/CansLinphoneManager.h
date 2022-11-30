8ce8826b60f756760385804ec75a9349a466ea11
phucanscloud <90325587+phucanscloud@users.noreply.github.com>
2022-11-30 18:06:19 +0700

    DONE: Added TestManager

--- CansConnect/CansConnect/CansLinphoneManager.h
+++ CansConnect/CansConnect/CansLinphoneManager.h
@@ -7,7 +7,9 @@
 
 #import <Foundation/Foundation.h>
 
-//#include "linphone/factory.h"
+#import "TestManager.h"
+
+#include "linphone/factory.h"
 //#include "linphone/linphonecore.h"
 
 //#import "linphone-sdk/apple-darwin/Frameworks/linphone.framework/Headers/linphonecore.h"
---
+++ CansConnect/CansConnect/TestManager.m
@@ -0,0 +1,12 @@
+//
+//  TestManager.m
+//  CansConnect
+//
+//  Created by Siraphop Chaisirikul on 30/11/2565 BE.
+//
+
+#import "TestManager.h"
+
+@implementation TestManager
+
+@end

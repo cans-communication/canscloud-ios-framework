ab33d54d116ea955cf74c8ab18f370e93db9837b
phucanscloud <90325587+phucanscloud@users.noreply.github.com>
2022-12-02 12:17:04 +0700

    WIP: Updated header project

--- CansConnect/CansConnect/CansConnect.h
+++ CansConnect/CansConnect/CansConnect.h
@@ -5,19 +5,8 @@
 //  Created by Siraphop Chaisirikul on 22/11/2565 BE.
 //
 
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#else
-#ifndef FOUNDATION_EXPORT
-#if defined(__cplusplus)
-#define FOUNDATION_EXPORT extern "C"
-#else
-#define FOUNDATION_EXPORT extern
-#endif
-#endif
-#endif
 
-//#import <Foundation/Foundation.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for CansConnect.
 FOUNDATION_EXPORT double CansConnectVersionNumber;

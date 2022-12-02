3b88736542f7497604c07dddd2fde97125085af2
phucanscloud <90325587+phucanscloud@users.noreply.github.com>
2022-12-02 14:54:50 +0700

    WIP: Re-path finder

--- CansConnect/CansConnect/CansConnect.h
+++ CansConnect/CansConnect/CansConnect.h
@@ -17,5 +17,4 @@ FOUNDATION_EXPORT const unsigned char CansConnectVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <CansConnect/PublicHeader.h>
 
 #import "LinphoneManager.h"
-
 #import "Alex.h"
